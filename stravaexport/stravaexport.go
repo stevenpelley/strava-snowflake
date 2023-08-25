e458a3bce8149f268021937266d480481335e8e7
stevenpelley <stevenpelley@gmail.com>
2023-08-25 19:02:42 -0400

    cleaning up stravaexport The server and requests now coordinate an orderly shutdown and process exit once we have exchanged and persisted the token.

--- stravaexport/stravaexport.go
+++ stravaexport/stravaexport.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
+	"log/slog"
 	"net"
 	"net/http"
 	"os"
@@ -16,7 +18,12 @@ import (
 
 const port = 8080 // port of local demo server
 
+// sent a struct{} once we have successfully shutdown the server, at which point
+// main may exit
+var shutdownChan chan struct{} = make(chan struct{})
+
 func main() {
+	initLogging()
 	var err error
 	// Try to bind address.  This allows subsequent requests, for example our own check that the
 	// server has started, to buffer.
@@ -28,17 +35,40 @@ func main() {
 
 	conf, err := createOauthConfig()
 	if err != nil {
-		panic(fmt.Sprintf("error creating oauth config. %v", err))
+		log.Panicf("error creating oauth config. %v", err)
 	}
 
 	go func() {
 		err := initiateOauthRequest(conf)
 		if err != nil {
-			panic(fmt.Sprintf("error while initiating oauth request: %v", err))
+			log.Panicf("error while initiating oauth request: %v", err)
 		}
 	}()
 	// blocks indefinitely
-	log.Fatal(listenAndServeOauthRedirection(listener))
+
+	err = listenAndServeOauthRedirection(listener, conf)
+	if err == http.ErrServerClosed {
+		// this is the expected case
+		slog.Info("server done listening and returned with ErrServerClosed (expected).  " +
+			"We will wait for shutdown to return")
+		<-shutdownChan
+		slog.Info("server shutdown return signalled.  Goodbye.")
+	} else {
+		slog.Error("unexpected return from server.listen", "error", err)
+		os.Exit(1)
+	}
+}
+
+func initLogging() {
+	// If the file doesn't exist, create it or append to the file
+	file, err := os.OpenFile("stravaexport.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger := slog.New(slog.NewJSONHandler(file, nil))
+	slog.SetDefault(logger)
 }
 
 // Create the oauth config used for all oauth requests
@@ -46,7 +76,7 @@ func createOauthConfig() (*oauth2.Config, error) {
 	var err error
 	data, err := os.ReadFile("oauth_client_config.json")
 	if err != nil {
-		panic(fmt.Sprintf("error reading the oauth client config: %v", err))
+		log.Panicf("error reading the oauth client config: %v", err)
 	}
 	type ClientConfig struct {
 		ClientId     string
@@ -96,7 +126,7 @@ func initiateOauthRequest(conf *oauth2.Config) error {
 	if err != nil {
 		return fmt.Errorf("error waiting for http server to start: %w", err)
 	}
-	fmt.Println("http server started. resp", resp)
+	slog.Info("confirmed http server started", "response", resp)
 
 	// open a browser and head to strava oauth
 	url := conf.AuthCodeURL("state")
@@ -106,22 +136,87 @@ func initiateOauthRequest(conf *oauth2.Config) error {
 
 // Starts a server, using a previously-created Listener, that will handle the redirect
 // from Strava oauth
-func listenAndServeOauthRedirection(listener net.Listener) error {
+func listenAndServeOauthRedirection(listener net.Listener, conf *oauth2.Config) error {
 	// create a server and listen with the previously created listener.  This will
 	// server any prior requests that buffered.
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("incoming request", r)
-			var err error = r.ParseForm()
-			if err != nil {
-				fmt.Println("error parsing form", err, "request", r)
-				w.Write([]byte(fmt.Sprintf("Error parsing the form: %v", err)))
-			} else {
-				fmt.Println("parsed the form.", "form", r.Form, "PostForm", r.PostForm)
-				w.Write([]byte(fmt.Sprintf("parsed the form. form: %v. PostForm: %v", r.Form, r.PostForm)))
-			}
-		}),
-	}
+	s := &http.Server{}
+	s.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("confirmed http server started")
+		var err error = r.ParseForm()
+		if err != nil {
+			slog.Error("error parsing form", "error", err)
+			w.Write([]byte(fmt.Sprintf("Error parsing the form: %v", err)))
+			return
+		}
 
-	return s.Serve(listener) // actually start the server, it manges all buffered requests
+		slog.Info("parsed the form", "form", r.Form, "PostForm", r.PostForm)
+		code := r.FormValue("code")
+		if code == "" {
+			slog.Info("'code' does not exist in form", "Form", r.Form)
+			w.Write([]byte(fmt.Sprintf("parsed the form but 'code' does not exist. "+
+				"form: %v. PostForm: %v", r.Form, r.PostForm)))
+			return
+		}
+		var token *oauth2.Token
+		token, err = conf.Exchange(context.Background(), code)
+		if err != nil {
+			slog.Error("error exchanging code for token", "error", err, "Form", r.Form)
+			w.Write([]byte(fmt.Sprintf("Error exchanging code for token: %v. form: %v",
+				err, r.Form)))
+			return
+		}
+		slog.Info("oauth exchange succeeded")
+		tokenBytes, err := json.MarshalIndent(token, "", "  ")
+		if err != nil {
+			slog.Error("error marshalling token", "error", err)
+			w.Write([]byte(fmt.Sprintf("Error marshalling token: %v. form: %v",
+				err, r.Form)))
+			return
+		}
+
+		tokenString := string(tokenBytes[:])
+		slog.Info("successfully exchanged code for token", "token", tokenString)
+		w.Write([]byte(fmt.Sprintf("token: %s", tokenString)))
+
+		// we write this to stdout so that it can be redirected to a file
+		// and stored for future use
+		file, err := os.OpenFile("token.txt",
+			os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+		if err != nil {
+			slog.Error("error opening token.txt file to write token", "error", err)
+			w.Write([]byte(fmt.Sprintf("Error opening token.txt to write"+
+				"token. error: %v. token: %s", err, tokenString)))
+			return
+		}
+		defer file.Close()
+		fmt.Fprint(file, tokenString)
+
+		// we have completed our token exchange and provided/persisted the token.  Shut down
+		// the server so that we can end the process.
+		go func() {
+			err := s.Shutdown(context.Background())
+			if err != nil {
+				slog.Error("shutting down server failed", "error", err)
+				// kill it with fire
+				os.Exit(-1)
+			}
+			slog.Info("successfully shut down the server")
+			shutdownChan <- struct{}{}
+		}()
+
+		////client := conf.Client(context.Background(), token)
+		////stravaApiConfig := stravaapi.NewConfiguration()
+		////stravaApiConfig.HTTPClient = client
+		////stravaClient := stravaapi.NewAPIClient(stravaApiConfig)
+		////detailedAthlete, resp, err := stravaClient.AthletesApi.GetLoggedInAthleteExecute(
+		////	stravaClient.AthletesApi.GetLoggedInAthlete(context.Background()))
+		////if err != nil {
+		////	slog.Error("error retrieving athlete", "error", err)
+		////	w.Write([]byte(fmt.Sprintf("Error getting Athlete: %v", err)))
+		////	return
+		////}
+		////w.Write([]byte(fmt.Sprintf("detailed athlete: %+v. resp: %v", detailedAthlete, resp)))
+	})
+
+	return s.Serve(listener) // actually start the server, it serves all buffered requests
 }
